e2e47d7182d5c1c166d36f6b6c6a6018854378d2
COMkey <rladmsdud4025@naver.com>
2025-01-29 00:35:31 +0900

    문체 변환하는 작업까지 작성

--- 2024_winter_sig/NLP_processing/main.py
+++ 2024_winter_sig/NLP_processing/main.py
@@ -1,1 +1,8 @@
-print("hello NLP")
+from nlp_processor import load_KoBART, generate_text
+
+voice_text = ""
+text_style = ""
+
+diary_text = generate_text(load_KoBART(), voice_text, text_style)
+
+#핵심문장 추출하는 부분 해야함.
