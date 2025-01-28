70461d7554b4aa65da1b6dcd8b73360ab7cbba50
COMkey <rladmsdud4025@naver.com>
2025-01-29 00:34:26 +0900

    BART 로드 및 일기 생성에 필요한한 함수 작성

---
+++ 2024_winter_sig/NLP_processing/nlp_processor.py
@@ -0,0 +1,38 @@
+import sys
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+def load_KoBART():
+    model_path = "C:\\Users\\rladm\\Desktop\\BARTmodel"
+    bart_model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    nlp_pipeline = pipeline('text2text-generation', model=bart_model, tokenizer=tokenizer)
+    return nlp_pipeline
+
+def generate_text(pipe, text, target_style, num_return_sequences=1, max_length=60):
+    text = f"{target_style} 말투로 변환 :{text}"
+    out = pipe(text, num_return_sequences=num_return_sequences, max_length=max_length)
+    return [x['generated_text'] for x in out][0]
+
+
+# 참고 (스타일 맵)
+# style_map = {
+#     'formal': '문어체',
+#     'informal': '구어체',
+#     'android': '안드로이드',
+#     'azae': '아재',
+#     'chat': '채팅',
+#     'choding': '초등학생',
+#     'emoticon': '이모티콘',
+#     'enfp': 'enfp',
+#     'gentle': '신사',
+#     'halbae': '할아버지',
+#     'halmae': '할머니',
+#     'joongding': '중학생',
+#     'king': '왕',
+#     'naruto': '나루토',
+#     'seonbi': '선비',
+#     'sosim': '소심한',
+#     'translator': '번역기'
+# }
