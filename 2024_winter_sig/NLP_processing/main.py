c0401d0da961b11fd056fb12894080436cc3a848
COMkey <rladmsdud4025@naver.com>
2025-02-07 03:26:59 +0900

    핵심문장추출코드까지 완성

--- 2024_winter_sig/NLP_processing/main.py
+++ 2024_winter_sig/NLP_processing/main.py
@@ -1,8 +1,9 @@
-from nlp_processor import load_KoBART, generate_text
+from nlp_processor import load_KoBART, generate_text, extract_keysents
 
 voice_text = ""
+
 text_style = ""
 
 diary_text = generate_text(load_KoBART(), voice_text, text_style)
 
-#핵심문장 추출하는 부분 해야함.
+finals_text = extract_keysents(voice_text)
